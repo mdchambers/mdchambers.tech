8dc3f5ee7547990be7e4ad37d8c761df2aacafe3
Michael Chambers <mike@mdchambers.com>
2019-05-09 01:40:46 -0400

    Homepage updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,65 @@ export default () => (
         <section className="hero is-success heroGradient is-medium">
             <div className="hero-body header">
                 <h1 className="title">Michael D Chambers</h1>
-                <h2 className="subtitle">Data analyst / full-stack web developer</h2>
+                <h2 className="subtitle">data analyst / full-stack web developer</h2>
+            </div>
+        </section>
+        <section>
+            <div className="container main-body">
+                <div className="columns is-multiline">
+                    <div className="column about-me is-full">
+                        <article className="media">
+                            <div className="media-content">
+                                <div className="content">
+                                    <h1 className="title is-size-4">About Me</h1>
+                                    <p className="subtitle is-size-6">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                        sed do eiusmod tempor incididunt ut labore et dolore magna 
+                                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div className="column is-one-third">
+                        <article className="media">
+                            <div className="media-content">
+                                <div className="content">
+                                    <h1 className="title is-size-4">Resume</h1>
+                                    <p className="subtitle is-size-5">
+                                        Download my resume
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div className="column is-one-third">
+                        <article className="media">
+                            <div className="media-content">
+                                <div className="content">
+                                    <h1 className="title is-size-4">Portfolio</h1>
+                                    <p className="subtitle is-size-5">
+                                        Investigate my portfolio
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>                
+                    <div className="column is-one-third">
+                        <article className="media">
+                            <div className="media-content">
+                                <div className="content">
+                                    <h1 className="title is-size-4">Social Media</h1>
+                                    <p className="is-size-5">
+                                        github
+                                        <br/>
+                                        linkedin
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
             </div>
         </section>
         {/* <img className="profilePic" src={pic} alt="Profile"></img>                 */}
