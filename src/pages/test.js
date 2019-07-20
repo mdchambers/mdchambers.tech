c182a387484a07a65b0a84ebff3528d69d4acb3b
Michael Chambers <mchambe0@ucla.edu>
2019-07-20 18:27:04 -0400

    fixing css

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React from "react"
 
-import './test.scss';
+import styles from "./test.module.scss"
 
 const test = () => {
   return (
     <React.Fragment>
-    <ul>
-      <li><a href="#foo">foo</a></li>
-      <li><a href="#bar">bar</a></li>
-      <li><a href="#baz">baz</a></li>
-      <li><a href="#qux">qux</a></li>
-    </ul>
-    <div className="container">
-      <div id="foo">foo</div>
-      <div id="bar">bar</div>
-      <div id="baz">baz</div>
-      <div id="qux">qux</div>
-    </div>
+      <ul>
+        <li>
+          <a href="#foo">foo</a>
+        </li>
+        <li>
+          <a href="#bar">bar</a>
+        </li>
+        <li>
+          <a href="#baz">baz</a>
+        </li>
+        <li>
+          <a href="#qux">qux</a>
+        </li>
+      </ul>
+      <div className={styles.container}>
+        <div id="foo">foo</div>
+        <div id="bar">bar</div>
+        <div id="baz">baz</div>
+        <div id="qux">qux</div>
+      </div>
     </React.Fragment>
   )
 }
